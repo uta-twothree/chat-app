9f2ff8319eb906cc0a537c26be759d12ef7aa56f
uta-twothree <yuta_ftm0718@icloud.com>
2021-05-20 22:46:03 +0900

    メッセージ送信機能の実装2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,7 +13,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
-      render index
+      render :index
     end
   end
 
